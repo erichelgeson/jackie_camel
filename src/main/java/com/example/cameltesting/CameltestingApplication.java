1a465d3c2b5cd9e7c8277e697ec0c431bfa08e1b
Eric Helgeson <erichelgeson@gmail.com>
2021-02-16 16:43:21 -0600

    Clean boot app

---
+++ src/main/java/com/example/cameltesting/CameltestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cameltesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameltestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CameltestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cameltesting/CameltestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cameltesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CameltestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
