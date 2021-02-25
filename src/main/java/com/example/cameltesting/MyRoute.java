de9f6b263a2b3a8d7f4118509f39fcab8e117ce6
Eric Helgeson <erichelgeson@gmail.com>
2021-02-25 08:27:25 -0600

    WIP

--- src/main/java/com/example/cameltesting/MyRoute.java
+++ src/main/java/com/example/cameltesting/MyRoute.java
@@ -6,11 +6,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyRoute extends RouteBuilder {
 
+// Testing constructor DI
+//    final MyProcessor myProcessor;
+//
+//    public MyRoute(MyProcessor myProcessor) {
+//        this.myProcessor = myProcessor;
+//    }
+
     @Override
     public void configure() throws Exception {
         from("direct:start")
                 .bean(MyProcessor.class)
-                .to("mock:direct:end");
+                .to("mock:cmis:foo")
+                .to("mock:result");
     }
 }
 
--- src/test/java/com/example/cameltesting/CameltestingApplicationTests.java
+++ src/test/java/com/example/cameltesting/CameltestingApplicationTests.java
@@ -1,28 +1,63 @@
 package com.example.cameltesting;
 
-import org.apache.camel.EndpointInject;
-import org.apache.camel.ProducerTemplate;
+import org.apache.camel.*;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
-import org.apache.camel.test.spring.junit5.MockEndpoints;
+import org.apache.camel.test.ExcludingPackageScanClassResolver;
+import org.apache.camel.test.spring.junit5.CamelSpringTestSupport;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@CamelSpringBootTest
-@SpringBootApplication
-@MockEndpoints("direct:end")
-class CamelRestingApplicationTests {
-	@Autowired
-	private ProducerTemplate template;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.context.junit.jupiter.SpringExtension.getApplicationContext;
 
-	@EndpointInject("mock:direct:end")
-	private MockEndpoint mock;
+class CameltestingApplicationTests extends CamelSpringTestSupport {
+
+	@EndpointInject("mock:result")
+	private MockEndpoint resultEndpoint;
+
+	@EndpointInject("mock:cmis:foo")
+	private MockEndpoint jdbcEndpoint;
+
+	@Produce("direct:start")
+	protected ProducerTemplate template;
+
+	@MockBean
+	MyProcessor myProcessor;
 
 	@Test
 	public void testReceive() throws Exception {
-		template.sendBody("direct:start", "Hello");
-		mock.expectedBodiesReceived("HELLO");
-		mock.assertIsSatisfied();
+		Exchange ex = createExchangeWithBody("Hello World");
+		resultEndpoint.expectedBodiesReceived("HELLO WORLD");
+		jdbcEndpoint.expectedMessageCount(1);
+
+		template.send(ex);
+
+//		verify(myProcessor, times(1)).process(any(Exchange.class));
+
+		resultEndpoint.assertIsSatisfied();
+		jdbcEndpoint.assertIsSatisfied();
 	}
+
+	@Override
+	protected RoutesBuilder[] createRouteBuilders() throws Exception {
+		// This is wrong as it's newing up MyRoute so not using the DI one from spring
+		return new RoutesBuilder[] {
+				new MyRoute()
+		};
+	}
+
+	@Override
+	protected AbstractApplicationContext createApplicationContext() {
+		GenericApplicationContext app =  new GenericApplicationContext();
+		return app;
+	}
+
 }
