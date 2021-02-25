52c4ee3ae26ff13d1da627f28d0ea6a8b0fda0bc
Eric Helgeson <erichelgeson@gmail.com>
2021-02-25 09:31:25 -0600

    Working boot

--- src/test/java/com/example/cameltesting/CameltestingApplicationTests.java
+++ src/test/java/com/example/cameltesting/CameltestingApplicationTests.java
@@ -3,12 +3,15 @@ package com.example.cameltesting;
 import org.apache.camel.*;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.ExcludingPackageScanClassResolver;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.apache.camel.test.spring.junit5.CamelSpringTestSupport;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
@@ -18,7 +21,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.context.junit.jupiter.SpringExtension.getApplicationContext;
 
-class CameltestingApplicationTests extends CamelSpringTestSupport {
+@CamelSpringBootTest
+@SpringBootApplication
+class CameltestingApplicationTests {
 
 	@EndpointInject("mock:result")
 	private MockEndpoint resultEndpoint;
@@ -34,30 +39,14 @@ class CameltestingApplicationTests extends CamelSpringTestSupport {
 
 	@Test
 	public void testReceive() throws Exception {
-		Exchange ex = createExchangeWithBody("Hello World");
-		resultEndpoint.expectedBodiesReceived("HELLO WORLD");
+		resultEndpoint.expectedBodiesReceived("Hello World"); // Processor is mocked so not upper case
 		jdbcEndpoint.expectedMessageCount(1);
 
-		template.send(ex);
-
-//		verify(myProcessor, times(1)).process(any(Exchange.class));
+		template.sendBody("Hello World");
+		verify(myProcessor, times(1)).process(any(Exchange.class)); // Mock is called
 
 		resultEndpoint.assertIsSatisfied();
 		jdbcEndpoint.assertIsSatisfied();
-	}
-
-	@Override
-	protected RoutesBuilder[] createRouteBuilders() throws Exception {
-		// This is wrong as it's newing up MyRoute so not using the DI one from spring
-		return new RoutesBuilder[] {
-				new MyRoute()
-		};
-	}
-
-	@Override
-	protected AbstractApplicationContext createApplicationContext() {
-		GenericApplicationContext app =  new GenericApplicationContext();
-		return app;
 	}
 
 }
